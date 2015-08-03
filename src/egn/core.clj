a4ddcc23d90ebc842150bb2e6d526042e1958975
gmitrev <gvmitrev@gmail.com>
2015-08-03 17:14:59 +0300

    Core convenience methods added

--- src/egn/core.clj
+++ src/egn/core.clj
@@ -1,1 +1,13 @@
-(ns egn.core)
+(ns egn.core
+  (:require [egn.validator :only [validate]]
+            [egn.parser    :only [parse]]))
+
+(defn validate
+  "Shortcut"
+  [egn]
+  (egn.validator/validate egn))
+
+(defn parse
+  "Shortcut"
+  [egn]
+  (egn.parser/parse egn))
